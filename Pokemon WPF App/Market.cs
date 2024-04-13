a28ba3b430cb7a870c1ca02e15227685b59f7428
Andrew M <admejia@ksu.edu>
2024-04-13 14:54:12 -0500

    Little bit of filtering and a little bit of 'market'

---
+++ Pokemon WPF App/Market.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_WPF_App
+{
+    public class MarketCards : INotifyPropertyChanged
+    {
+        private ObservableCollection<Card> _marketCards;
+        private ObservableCollection<Card> _filteredMarketCards;
+
+        public ObservableCollection<Card> MC
+        {
+            get { return _marketCards; }
+            set
+            {
+                _marketCards = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<Card> FilteredMarketCards
+        {
+            get { return _filteredMarketCards; }
+            set
+            {
+                _filteredMarketCards = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MarketCards()
+        {
+            MC = new ObservableCollection<Card>();
+            FilteredMarketCards = new ObservableCollection<Card>();
+        }
+
+        public void AddCard(Card card)
+        {
+            MC.Add(card);
+            FilteredMarketCards.Add(card); // Add to filtered list by default
+        }
+
+        // Implement INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
+
+
+
+
--- Pokemon WPF App/MarketPage.xaml.cs
+++ Pokemon WPF App/MarketPage.xaml.cs
@@ -16,19 +16,88 @@ using System.Windows.Shapes;
 
 namespace Pokemon_WPF_App
 {
-    public class MarketViewModel
+    public partial class MarketPage : Page
     {
-        public ObservableCollection<Card> Cards { get; set; }
+        private MarketCards market;
 
-            public MarketViewModel()
+        public MarketPage()
+        {
+            InitializeComponent();
+
+            // Create a new Market object
+            market = new MarketCards();
+
+            // Add some sample cards to the market
+            // ...
+            for (int i = 0; i < 30; i++)
             {
-                // Initialize the collection and add some sample data
-                Cards = new ObservableCollection<Card>
+                market.AddCard(new Card
                 {
-                    new Card { Name = "Card1", Description = "Description1" },
-                    new Card { Name = "Card2", Description = "Description2" },
-                    // Add more cards as needed
-                };
+                    Name = "Charizard " + i,
+                    Description = "A Fire/Flying type Pokémon",
+                    ImagePath = "/Images/Charizard.png",
+                    Type = "Fire/Flying",
+                    HP = 78,
+                    Attack = 84,
+                    Defense = 78
+                });
             }
+            for (int i = 0; i < 30; i++)
+            {
+                market.AddCard(new Card
+                {
+                    Name = "Squirtle" + i,
+                    Description = "A Fire/Flying type Pokémon",
+                    ImagePath = "/Images/Charizard.png",
+                    Type = "Fire/Flying",
+                    HP = 78,
+                    Attack = 84,
+                    Defense = 78
+                });
+            }
+
+            // Set the data context for the MarketPage
+            DataContext = market;
+
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = SearchTextBox.Text.Trim().ToLower();
+
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                // If search term is empty or whitespace, show all cards
+                market.FilteredMarketCards = new ObservableCollection<Card>(market.MC);
+            }
+            else
+            {
+                // Filter the market cards based on the search term
+                market.FilteredMarketCards = new ObservableCollection<Card>(
+                    market.MC.Where(card => card.Name.ToLower().Contains(searchTerm)));
+            }
+        }
+
+        private void EnergyTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (EnergyTypeComboBox.SelectedItem != null)
+            {
+                string selectedEnergyType = ((ComboBoxItem)EnergyTypeComboBox.SelectedItem).Content.ToString();
+
+                if (selectedEnergyType == "All")
+                {
+                    // Show all cards
+                    market.FilteredMarketCards = new ObservableCollection<Card>(market.MC);
+                }
+                else
+                {
+                    // Filter the market cards based on the selected energy type
+                    market.FilteredMarketCards = new ObservableCollection<Card>(
+                        market.MC.Where(card => card.Type.ToLower().Contains(selectedEnergyType.ToLower())));
+                }
+            }
+        }
+
+
     }
 }
